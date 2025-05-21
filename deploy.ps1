120ab94869d61bbbd41f64c1724414772e52e099
Denis Morielli <denis.morielli@opiu.ca>
2025-05-20 22:16:04 -0400

    Edited Deploy file to deploy on local temporary folder.

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,6 @@
 Set-Location $PSScriptRoot
 
-Connect-AzAccount -TenantId "<YOUR TENANT ID>" -Subscription "<YOUR SUBSCRIPTION ID>" -InformationAction Ignore
+Connect-AzAccount -TenantId "<YOUR TENANT ID>" -Subscription "<YOUR SUBSCRIPTION ID>"
 
 ## Deploy Infra
 <##>
@@ -11,13 +11,18 @@ $deployment = New-AzSubscriptionDeployment -Name "DEMO_AVM" `
     -TemplateParameterFile biceps\params\main.bicepparam
 
 
+## Setup local installation folders
+$tempInstallFolder = "$([System.IO.Path]::GetTempPath())$(New-Guid)"
+New-Item -ItemType Directory -Path $tempInstallFolder -Force | Out-Null
+$tempWfPath = (New-Item -ItemType Directory -Path $tempInstallFolder\wf -Force).FullName
+$tempFxPath = (New-Item -ItemType Directory -Path $tempInstallFolder\fx -Force).FullName
+
 ## Deploy the Function App and the Logic App Standard Workflow for the demo
 $rgName = $deployment.Outputs.functionAppResourceGroupName.Value
 $laName = $deployment.Outputs.logicAppName.Value
 $fxName = $deployment.Outputs.functionAppName.Value
 $fxEndpoint = "https://$fxName.azurewebsites.net/api/CallFunction"
 $uaiResourceId = $deployment.Outputs.userAssignedManagedIdentityId.Value
-
 
 <#
 NOTE:
@@ -27,27 +32,26 @@ We re-enable it after the deployment.
 This is, of course, not recommended except for testing purposes.
 #>
 Write-Host "Temporarly disable network isolation..." -ForegroundColor Magenta
-
 $fx = Get-AzResource -Name $fxName -ResourceGroupName $rgName -ResourceType Microsoft.Web/sites
 $fx.Properties.publicNetworkAccess = "Enabled"
 $fx | Set-AzResource -Force | Out-Null
 
 # Logic App Standard
 Write-Host "Deploy the LogicApp workflow..." -ForegroundColor Magenta
-Get-ChildItem -Path .\apps\workflow | Copy-Item -Destination .\.pub\wf -Recurse -Force
-$parameters = Get-Content -Path .\.pub\wf\parameters.json -Raw
+Get-ChildItem -Path .\apps\workflow | Copy-Item -Destination $tempWfPath -Recurse -Force
+$parameters = Get-Content -Path $tempWfPath\parameters.json -Raw
 $parameters = $parameters -replace "#__userAssignedIdentityId__#", $uaiResourceId -replace "#__functionAppEndpoint__#", $fxEndpoint
-$parameters | Set-Content -Path .\.pub\wf\parameters.json -Force
+$parameters | Set-Content -Path $tempWfPath\parameters.json -Force
 
-Compress-Archive -Path .\.pub\wf\* -DestinationPath .pub\wf\workflow.zip -Force
-Publish-AzWebApp -Name $laName -ResourceGroupName $rgName -ArchivePath .pub\wf\workflow.zip -Force | Out-Null
+Compress-Archive -Path $tempWfPath\* -DestinationPath $tempWfPath\workflow.zip -Force
+Publish-AzWebApp -Name $laName -ResourceGroupName $rgName -ArchivePath $tempWfPath\workflow.zip -Force | Out-Null
 
 # Function App
 Write-Host "Deploy the FunctionApp function..." -ForegroundColor Magenta
 dotnet build apps\function\fx.sln --configuration Release
-dotnet publish apps\function\fx.csproj --configuration Release --output .\.pub\fx
-Compress-Archive -Path .pub\fx\* -DestinationPath .pub\fx\function.zip -Force
-Publish-AzWebApp -Name $fxName -ResourceGroupName $rgName -ArchivePath .pub\fx\function.zip -Force | Out-Null
+dotnet publish apps\function\fx.csproj --configuration Release --output $tempFxPath
+Compress-Archive -Path $tempFxPath\* -DestinationPath $tempFxPath\function.zip -Force
+Publish-AzWebApp -Name $fxName -ResourceGroupName $rgName -ArchivePath $tempFxPath\function.zip -Force | Out-Null
 
 # Restore isolation from public networks
 Write-Host "Restore network isolation..." -ForegroundColor Magenta
@@ -56,5 +60,4 @@ $fx | Set-AzResource -Force | Out-Null
 
 # Clean-up app packages
 Write-Host "Clean-up..." -ForegroundColor Magenta
-Remove-Item -Path .pub\wf\* -Recurse -Force
-Remove-Item -Path .pub\fx\* -Recurse -Force
+Remove-Item -Path $tempInstallFolder -Recurse -Force
